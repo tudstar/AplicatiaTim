182296b28e983ae3ee0b7fb4152c0f6b44ae9854
tuds <tudy.tanasciuc@gmail.com>
2019-09-08 11:00:11 +0300

    First Commit

---
+++ app/src/main/java/com/example/cityapp/Activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.cityapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cityapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
