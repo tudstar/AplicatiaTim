71dcee4b46e1c8ccee4c49fe51e127b19b869fc6
tuds <tudy.tanasciuc@gmail.com>
2019-09-08 13:54:18 +0300

    third commit

--- app/src/main/java/com/example/cityapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/cityapp/Activities/RegisterActivity.java
@@ -1,16 +1,93 @@
 package com.example.cityapp.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.cityapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    ImageView ImgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESTCODE = 1;
+    Uri pickedImgURI ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //inu views
+
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+
+                                                if (Build.VERSION.SDK_INT >= 22) {
+                                                    ceckAndRequestForPermission();
+
+                                                } else {
+                                                    openGallery();
+                                                }
+                                            }
+                                        }
+        );
+    }
+
+    private void openGallery() {
+        //TODO: open gallery intent and wait for user to pick an image
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESTCODE);
+
+
+    }
+
+    private void ceckAndRequestForPermission() {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(RegisterActivity.this, "Please accept for required permission", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+
+            }
+        } else {
+            openGallery();
+
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUESTCODE && data !=null) {
+            //the user has succesfully picked a image
+            //we need to save its reference to a variable
+            pickedImgURI = data.getData() ;
+            ImgUserPhoto.setImageURI(pickedImgURI);
+        }
+
     }
 }
+
+
+
+
+
